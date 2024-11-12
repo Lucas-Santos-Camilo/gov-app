bdfec550f8565863094c99cfe39c35ac97aee8dd
Lucas dos Santos Camilo <lucasdossantosnet@gmail.com>
2024-11-12 13:55:20 -0300

    estilização

--- gov-app/src/pages/Acesso.js
+++ gov-app/src/pages/Acesso.js
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { login } from '../api';
-import './Style.css';
+import '../pages/Style.css';
 
 function Acesso() {
   const navigate = useNavigate();
